dc5cfe5d46569d620dbeedd03f6c6d48b0967ee0
vvattap <63315944+vvattap@users.noreply.github.com>
2020-12-19 12:41:58 -0500

    Add files via upload

---
+++ Calc.h
@@ -0,0 +1,9 @@
+int _add(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int neg(int a);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+int pow(int a, int b);
+int convert(char *input);
